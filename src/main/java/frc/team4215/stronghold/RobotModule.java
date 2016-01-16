02443658f75441ac8af6e28b66ec62f38be43dfb
Jack-Rausch <jack.t.rausch@gmail.com>
2016-01-16 15:11:52 -0600

    Implemented UltraProto

--- src/main/java/frc/team4215/stronghold/RobotModule.java
+++ src/main/java/frc/team4215/stronghold/RobotModule.java
@@ -1,5 +1,7 @@
 package frc.team4215.stronghold;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogOutput;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import jaci.openrio.toast.lib.log.Logger;
 import 
@@ -23,17 +25,36 @@ public class RobotModule extends IterativeModule {
         return ModuleVersion;
     }
     Ultrasonic ultra = new Ultrasonic(1,1);
+    AnalogInput anInput = new AnalogInput(1);
+	AnalogOutput anOutput = new AnalogOutput(1);
+    double voltRange = 5.0;
+	double distanceRange;
+	double range;
+	double avrange;
     
     public double getRange(){
-		double range = ultra.getRangeInches();
-		return range;}
+		double range = anInput.getVoltage();
+		//takes the analog voltage as a double
+		return ((range / voltRange)*distanceRange);
+		// converts voltage range into a percentage, then multiplies it by the overall distance to make it more understandable
+    }
+    
+    public double getAverageRange(){
+		double avrange = anInput.getAverageVoltage();
+		//takes the average analog voltage as a double
+		return ((avrange / voltRange)*distanceRange);
+		// converts average voltage range into a percentage, then multiplies it by the maximum distance to make it into understandable units
+	}
    
     @Override
     
     public void robotInit() {
         logger = new Logger("stronghold", Logger.ATTR_DEFAULT);
         double range = getRange();
-        System.out.println("Range is " + range);
+        String str_range = String.valueOf(range);
+        String str_avrange = String.valueOf(avrange);
+        logger.info("range is..." + range);
+        logger.info("average range is..." + avrange);
        
     }
 }
