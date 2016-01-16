11d2069dbd00c6c942320a312a5fb49cce308c9a
Jack-Rausch <jack.t.rausch@gmail.com>
2016-01-16 15:12:15 -0600

    Compatible with Analog Voltage

--- src/main/java/frc/team4215/stronghold/UltraProto.java
+++ src/main/java/frc/team4215/stronghold/UltraProto.java
@@ -1,40 +1,46 @@
 package frc.team4215.stronghold;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogOutput;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import jaci.openrio.toast.lib.module.IterativeModule;
 
 /**
-<<<<<<< HEAD
  * Prototype that measures distance as an analog voltage with UltraSonic sensor, then converts voltage into understandable units.
-=======
- * Prototype that measures distance with UltraSonic sensor, then converts data into a readable form.
->>>>>>> refs/remotes/origin/master
  * @author Jack Rausch
  *
  */
 public class UltraProto extends IterativeModule {
 	
 	Ultrasonic ultra = new Ultrasonic(1,1);
-<<<<<<< HEAD
 	// creates an object and assigns variable 'ultra' to be the UltraSonic sensor which uses Output1 for echo-pulse and Input1 for trigger-pulse
-=======
-	// creates an object and assigns ultra to be an ultrasonic sensor which uses Output1 for echopulse and Input1 for triggerpulse
->>>>>>> refs/remotes/origin/master
+	AnalogInput anInput = new AnalogInput(1);
+	AnalogOutput anOutput = new AnalogOutput(1);
+
 	public void robotInit(){
 		ultra.setAutomaticMode(true);
 	//enables Automatic mode
 	}
 	
 	
-	double voltRange;
+	double voltRange = 5.0;
 	double distanceRange;
+	double range;
+	double avrange;
 	//defines variables for values to be inputed later
 	
 	public double getRange(){
-		double range = ultra.getVoltage(*inputchannel*);
+		double range = anInput.getVoltage();
 		//takes the analog voltage as a double
 		return ((range / voltRange)*distanceRange);
 		// converts voltage range into a percentage, then multiplies it by the overall distance to make it more understandable
+	}
+	
+	public double getAverageRange(){
+		double avrange = anInput.getAverageVoltage();
+		//takes the average analog voltage as a double
+		return ((avrange / voltRange)*distanceRange);
+		// converts average voltage range into a percentage, then multiplies it by the maximum distance to make it into understandable units
 	}
 	
 	@Override
