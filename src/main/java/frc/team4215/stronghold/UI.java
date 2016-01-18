643870bed7221a15639ecf9f5036922022ed9c0f
Waweros <lion@waposta.com>
2016-01-18 15:16:55 -0600

    Added more breadth to UI code

--- src/main/java/frc/team4215/stronghold/UI.java
+++ src/main/java/frc/team4215/stronghold/UI.java
@@ -3,19 +3,30 @@ package frc.team4215.stronghold;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class UI {
-	Joystick leftStick;
-	Joystick rightStick;
+	Joystick[] driveSticks = new Joystick[2];
+	Joystick thirdstick;
 
-	UI(Joystick leftStick_,Joystick rightStick_){
-		leftStick = leftStick_;
-		rightStick = rightStick_;
+	public UI(Joystick leftStick_,Joystick rightStick_, Joystick thirdstick_){
+		driveSticks[0] = leftStick_;
+		driveSticks[1] = rightStick_;
+		thirdstick = thirdstick_;
+	}
+	
+	public UI(Joystick driveStick_, Joystick thirdstick_){
+		driveSticks[0] = driveStick_;
+		thirdstick = thirdstick_;
 	}
 	
 	double[] getInputs(){
-		double[] inputs = {
-		leftStick.getRawAxis(0),
-		rightStick.getRawAxis(1)
-		};
+		double[] inputs = new double[2];
+		if(driveSticks.length == 1){
+			inputs[0] = driveSticks[0].getRawAxis(1);
+			inputs[1] = driveSticks[0].getRawAxis(5);
+		}
+		else{
+			inputs[0] = driveSticks[0].getRawAxis(1);
+			inputs[1] = driveSticks[1].getRawAxis(1);
+		}
 		return inputs;
 	}
 	
