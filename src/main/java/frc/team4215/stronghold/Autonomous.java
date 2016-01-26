ea4db66eee3da1769639a562616da2bd0b0d2fa4
James Y. <abcde201220122012@outlook.com>
2016-01-26 17:20:56 -0600

    updates

--- src/main/java/frc/team4215/stronghold/Autonomous.java
+++ src/main/java/frc/team4215/stronghold/Autonomous.java
@@ -1,6 +1,21 @@
+
 package frc.team4215.stronghold;
 
+import edu.wpi.first.wpilibj.Talon;
 
 public class Autonomous {
 
+    private Talon frontLeft, frontRight, backLeft, backRight;
+
+    public Autonomous(Talon frontLeft_, Talon frontRight_, Talon backLeft_,
+            Talon backRight_) {
+        this.frontLeft = frontLeft_;
+        this.frontRight = frontRight_;
+        this.backLeft = backLeft_;
+        this.backRight = backRight_;
+    }
+
+    public void Auto_Portcullis() {
+
+    }
 }
