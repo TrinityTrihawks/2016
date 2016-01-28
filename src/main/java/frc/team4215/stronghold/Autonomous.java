c747dc8bfc1b4cbac108f709f0861369ca7db1c2
James Y. <abcde201220122012@outlook.com>
2016-01-28 17:48:09 -0600

    typo fix

--- src/main/java/frc/team4215/stronghold/Autonomous.java
+++ src/main/java/frc/team4215/stronghold/Autonomous.java
@@ -63,7 +63,7 @@ public class Autonomous {
          * 
          * @author Josh
          */
-        public static final class ConstLowBar {
+        private static final class ConstLowBar {
         }
         
         /**
