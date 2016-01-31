082074fb4c8e395e29931a8f49cd00ce005c2d05
James Y. <abcde201220122012@outlook.com>
2016-01-31 12:31:26 -0600

    updates
    learning how to set it up.

--- src/main/java/frc/team4215/stronghold/RoboNet.java
+++ src/main/java/frc/team4215/stronghold/RoboNet.java
@@ -1,12 +1,37 @@
 
 package frc.team4215.stronghold;
 
+import java.io.IOException;
 import java.net.*;
 
+/**
+ * @author James
+ */
 public class RoboNet {
+
+    /*
+     * I'm still trying to find out what can be done with networking and what to
+     * do
+     */
     ServerSocket serverSocket;
+    Socket socket;
+    int errorCode;
+    String errorMessage;
+    public static final int port = 4215;
     
     public RoboNet() {
+        // Error Mode init
+        this.errorCode = 0;
+        this.errorMessage = null;
+        try {
+            this.serverSocket = new ServerSocket();
+            this.socket = new Socket();
+        } catch (IOException e) {
+            this.errorMessage = e.getMessage();
+        }
+    }
+    
+    public void connect() {
     
     }
 }
