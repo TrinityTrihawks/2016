0a99a69c715f963c251f4ed56c06adb380e41cd9
James Y <abcde201220122012@outlook.com>
2016-02-11 16:43:36 -0600

    found a bug..
    forgot to change the value back

--- src/main/java/frc/team4215/stronghold/Autonomous.java
+++ src/main/java/frc/team4215/stronghold/Autonomous.java
@@ -9,18 +9,18 @@ import edu.wpi.first.wpilibj.Victor;
  * @author James
  */
 public class Autonomous {
-
+    
     private Victor frontLeft, frontRight, backLeft, backRight,
             armMotor, intake;
     private Interface choiceAuto;
-    
+
     public Autonomous(Victor frontLeft_, Victor frontRight_,
             Victor backLeft_, Victor backRight_, Victor armMotor_,
             Victor intake_) throws RobotException {
         this(new Victor[] { frontLeft_, frontRight_, backLeft_,
                 backRight_, armMotor_, intake_ });
     }
-    
+
     public Autonomous(Victor[] sixVictors) throws RobotException {
         if (sixVictors.length < 6) throw new RobotException(
                 "Victor array's length is less than 6");
@@ -33,7 +33,7 @@ public class Autonomous {
         this.armMotor = a[4];
         this.intake = a[5];
     }
-    
+
     public void chooseAuto(int num) {
         if (num == 1) this.choiceAuto = () -> this.autoLowBar();
         else if (num == 2)
@@ -44,77 +44,77 @@ public class Autonomous {
             this.choiceAuto = () -> this.autoPortcullis();
         else this.choiceAuto = null;
     }
-    
+
     public void autoChoice() throws RobotException {
         if (null != this.choiceAuto)
             throw new RobotException("There is not a method chosen.");
         this.choiceAuto.runAuto();
     }
-
+    
     /**
      * All constants.
      *
      * @author James
      */
     private static final class Constant {
-
+        
         /**
          * Constant shared.
          *
          * @author James
          */
         public static final class Shared {
-
+            
             static final double armMoveMaxTime = 2d;
-
+            
             static final double armDown = -1, armUp = 1, armStop = 0;
-
+            
             public static final double intakeDelay = 1d;
         }
-
+        
         /**
          * Constant for autoLowBar.
          *
          * @author James
          */
         private static final class ConstLowBar {
-            
+
             public static final double driveThroughDelay = 5d;
         }
-
+        
         /**
          * Constant for autoSpyBotLowGoal.
          *
          * @author James
          */
         private static final class ConstSpyBotLowGoal {
-
+            
             public static final double driveToDelay = 5d;
         }
-
+        
         /**
          * Constant for autoChevalDeFrise.
          *
          * @author James
          */
         private static final class ConstChevalDeFrise {
-
+            
             public static final double driveToDelay = 5d;
             public static final double driveThroughDelay = 5d;
         }
-
+        
         /**
          * Constant for autoPortcullis.
          *
          * @author James
          */
         public static final class ConstPortcullis {
-
+            
             public static final double driveDelay = 5d;
             public static final double driveThroughDelay = 5d;
         }
     }
-
+    
     /**
      * The interface for programs outside to run the chosen autonomous
      * function.
@@ -122,10 +122,10 @@ public class Autonomous {
      * @author James
      */
     public interface Interface {
-
+        
         public void runAuto();
     }
-    
+
     /**
      * to lower arm. Need more info.
      *
@@ -136,7 +136,7 @@ public class Autonomous {
         Autonomous.delay(Constant.Shared.armMoveMaxTime);
         this.armMotor.set(Constant.Shared.armStop);
     }
-    
+
     /**
      * to delay for some time. Need more info.
      *
@@ -149,7 +149,7 @@ public class Autonomous {
         // stick to using Autonomous.delay.
         edu.wpi.first.wpilibj.Timer.delay(delayTime);
     }
-    
+
     /**
      * to lift arm. Need more info
      *
@@ -160,20 +160,21 @@ public class Autonomous {
         Autonomous.delay(Constant.Shared.armMoveMaxTime);
         this.armMotor.set(Constant.Shared.armStop);
     }
-    
+
     /**
      * to drive straight. Need more info.
      *
      * @author James
      */
     private void driveStraight(double driveTime) {
+        DriveTrain dT = new DriveTrain(this.frontLeft, this.backLeft,
+                this.frontRight, this.backRight);
         // command starts
-        new DriveTrain(this.frontLeft, this.backLeft, this.frontRight,
-                this.backRight).drive(Const.Motor.Run.Forward);
-
+        dT.drive(Const.Motor.Run.Forward);
         Autonomous.delay(driveTime);
+        dT.drive(Const.Motor.Run.Stop);
     }
-
+    
     /**
      * throw ball out. Yet tested.
      *
@@ -184,7 +185,7 @@ public class Autonomous {
         Autonomous.delay(Constant.Shared.intakeDelay);
         this.intake.set(Const.Motor.Run.Stop);
     }
-
+    
     /**
      * Autonomous function No.1
      *
@@ -194,7 +195,7 @@ public class Autonomous {
         this.armLowerBottom();
         this.driveStraight(Constant.ConstLowBar.driveThroughDelay);
     }
-    
+
     /**
      * Autonomous function No.2
      *
@@ -205,7 +206,7 @@ public class Autonomous {
         this.driveStraight(Constant.ConstSpyBotLowGoal.driveToDelay);
         this.throwBall();
     }
-    
+
     /**
      * Autonomous function No.3
      *
@@ -217,7 +218,7 @@ public class Autonomous {
         this.driveStraight(
                 Constant.ConstChevalDeFrise.driveThroughDelay);
     }
-    
+
     /**
      * Autonomous function No.4
      *
@@ -230,7 +231,7 @@ public class Autonomous {
         this.driveStraight(
                 Constant.ConstPortcullis.driveThroughDelay);
     }
-    
+
     /**
      * Should be equivalent to a method called getAccel of another
      * class I2CAccelerometer which isn't here yet.
@@ -242,7 +243,7 @@ public class Autonomous {
         double[] accel = new double[3];
         return accel; // placeholder
     }
-    
+
     /**
      * Should be equivalent to a method called getAngles of another
      * class I2CGyro which isn't here yet.
@@ -254,5 +255,5 @@ public class Autonomous {
         double[] angles = new double[3];
         return angles; // placeholder
     }
-
+    
 }
