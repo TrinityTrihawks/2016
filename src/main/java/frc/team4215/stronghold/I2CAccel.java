a70e956e9791a9d05e24e9b643db414279e05e1b
Waweros <lion@waposta.com>
2016-02-11 16:53:09 -0600

    Changed Accel

--- src/main/java/frc/team4215/stronghold/I2CAccel.java
+++ src/main/java/frc/team4215/stronghold/I2CAccel.java
@@ -1,16 +1,25 @@
 package frc.team4215.stronghold;
 
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.internal.HardwareTimer;
 
 public class I2CAccel {
 	static I2C accel;
 	
+	
 	final static byte WHO_AM_I = 0x0F,
 			  CTRL_REG = 0x20,
 			  OUT_REG = 0x28;
+	static byte[] bufferData = new byte[6];
 	
 	public void initAccel(){
 		accel = new I2C(I2C.Port.kOnboard, 0x1D);
+		
+	}
+	
+	public vvoid pingAccel(){
+		accel.read(OUT_REG,6,bufferData);
 		
 	}
 }
