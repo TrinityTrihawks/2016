0291f7bb282de9909b1cad79af7197c087a37fe0
Waweros <lion@waposta.com>
2016-02-19 16:43:38 -0600

    Added run method for Winch

--- src/main/java/frc/team4215/stronghold/Winch.java
+++ src/main/java/frc/team4215/stronghold/Winch.java
@@ -1,20 +1,29 @@
 package frc.team4215.stronghold;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Victor;
 import jaci.openrio.toast.lib.registry.Registrar;
 
 public class Winch {
 
     private Victor winch;
-
+    private Joystick gameCube;
+    private double coeff = .5;
     public Winch() {
         winch = Registrar.victor(Const.Motor.Num.Winch);
+        gameCube = new Joystick(Const.JoyStick.Num.GameCube);
     }
 
     public void setSafetyEnabled(boolean bool) {
         winch.setSafetyEnabled(bool);
     }
     
+    public void run(){
+    	
+     double val = gameCube.getRawAxis(Const.JoyStick.Button.GameCube_Y);
+     winch.set(coeff*val);
+    		
+    }
     public void set(double value) {
         winch.set(value);
     }
