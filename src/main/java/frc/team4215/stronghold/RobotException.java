6a6b165b6e178b0be47b2ff26ca977a48af01854
JAMESY9868 <abcde201220122012@outlook.com>
2016-02-19 18:02:09 -0600

    updates so taht it can output user-defined message.

--- src/main/java/frc/team4215/stronghold/RobotException.java
+++ src/main/java/frc/team4215/stronghold/RobotException.java
@@ -2,16 +2,22 @@
 package frc.team4215.stronghold;
 
 public class RobotException extends Exception {
-    
+
     /**
      * computer genrated serialVersionUID.
      */
     private static final long serialVersionUID =
             -6564268929214879778L;
-            
+
     public String exceptionString;
-    
+
     public RobotException(String exceptionString_) {
-        this.exceptionString = exceptionString_;
+        exceptionString = exceptionString_;
+    }
+    
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+        System.out.println(exceptionString);
     }
 }
