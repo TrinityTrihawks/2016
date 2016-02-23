8fc2c097239471f5aca00b689a352822750eb917
Waweros <lion@waposta.com>
2016-02-23 09:41:37 -0600

    FOund better static constant values for the coeff and made it output its data in ips^2

--- src/main/java/frc/team4215/stronghold/I2CAccel.java
+++ src/main/java/frc/team4215/stronghold/I2CAccel.java
@@ -5,16 +5,13 @@ import edu.wpi.first.wpilibj.I2C;
 public class I2CAccel {
 
     private static I2C accel;
-    private static int maxBit = 0xFFFF;
-    private static int range = 2;
-    private static double coeff = (double) range / maxBit;
+    private static double coeff = .061;
     private static boolean pingFlag;
     private final static byte WHO_AM_I = 0x0F, CTRL_REG = 0x1F,
             OUT_REG = 0x28;
-
+    private static double G_IN_IPS2 = 386.09;
     private static byte[] buffL = new byte[1], buffH = new byte[1],
             ID = new byte[1];
-    private static int offsetX, offsetY, offsetZ;
     private static double[] accelVal = new double[3];
     private static Thread pingerThread;
 
@@ -37,14 +34,20 @@ public class I2CAccel {
         accel.read(OUT_REG, 1, buffL);
         accel.read(OUT_REG + 1, 1, buffH);
         accelVal[0] = concatCorrect(buffH[0], buffL[0]);
-
+        accelVal[0] /= 1000;
+        accelVal[0] *= G_IN_IPS2;
+        
         accel.read(OUT_REG + 2, 1, buffL);
         accel.read(OUT_REG + 3, 1, buffH);
         accelVal[1] = concatCorrect(buffH[0], buffL[0]);
-
+        accelVal[1] /= 1000;
+        accelVal[1] *= G_IN_IPS2;
+        
         accel.read(OUT_REG + 4, 1, buffL);
         accel.read(OUT_REG + 5, 1, buffH);
         accelVal[2] = concatCorrect(buffH[0], buffL[0]);
+        accelVal[2] /= 1000;
+        accelVal[2] *= G_IN_IPS2;
 
     }
 
@@ -53,7 +56,7 @@ public class I2CAccel {
         int low = Byte.toUnsignedInt(l);
         int test = ((0xFF & high) << 8) + (0xFF & low);
         
-        return coeff * ((test > 32767) ? test - 65536 : test);
+        return coeff * ((test > 0x7FFF) ? test - 0xFFFF : test);
     }
 
     public static void pingerStart() {
