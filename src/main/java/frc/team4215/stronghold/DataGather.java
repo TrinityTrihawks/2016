a6d2c66cf9454c0c9776be4a8259d9838dd31c04
TheBest- <joeybricher@gmail.com>
2016-03-03 17:00:17 -0600

    Added other two axes to logger for gyroscope

--- src/main/java/frc/team4215/stronghold/DataGather.java
+++ src/main/java/frc/team4215/stronghold/DataGather.java
@@ -10,76 +10,100 @@ public class DataGather {
 	BlackBoxContext context;
 	DriveTrain chassis;
 	UI ds;
-	
+
 	public DataGather( DriveTrain chassis, UI ds) {
 		this.chassis = chassis;
 		this.ds = ds;
-		
+
 		context = BlackBox.context("gathered_data.csv");
-		
+
 		//  Logging the volts to the motors
 		context.add("Volts: Right ", this::leftVolts);
 		context.add("Volts: Left", this::rightVolts);
-		
+
 		// Logginng the inputs to the drive station
 		context.add("Drive: Left", this::leftInputs);
 		context.add("Drive: Right", this::rightInputs);
-		
+
 		// Logging Accelerations
 		context.add("Accel: Z", this::zAccel);
 		context.add("Accel: X data", this::xAccel);
 		context.add("Accel: Y data", this::yAccel);
-		
+
 		// Logging Angles and Angular speed from the Gyro
+		context.add("Angle: X ", this::xAngle);
+		context.add("Angular-Speed: X",this::xAngularSpeed);
+
+		context.add("Angle: Y ", this::yAngle);
+		context.add("Angular-Speed: Y",this::yAngularSpeed);
+
 		context.add("Angle: Z ", this::zAngle);
 		context.add("Angular-Speed: Z", this::zAngularSpeed);
 	}
-	
+
 	public void tick(){
 		context.tick();
 	}
-	
-	
+
+
 	public double leftInputs(){
 		double[] inputs = ds.getDriveInputs();
 		return inputs[0];
 	}
-	
+
 	public double rightInputs(){
 		double[] inputs = ds.getDriveInputs();
 		return inputs[1];
 	}
-	
+
 	public double leftVolts(){
 		double[] volts = chassis.getVoltages();
 		return volts[0];
 	}
-	
+
 	public double rightVolts(){
 		double[] volts = chassis.getVoltages();
 		return volts[4];
 	}
-	
+
 	public double zAccel(){
 		double[] accel = I2CAccel.getAccel();
 		return accel[2];
 	}
-	
+
 	public double xAccel(){
 		double[] accel = I2CAccel.getAccel();
 		return accel[0];
 	}
-	
+
 	public double yAccel(){
 		double[] accel = I2CAccel.getAccel();
 		return accel[1];
 	}
-	
+
+	public double xAngularSpeed(){
+		double[] speed = I2CGyro.getAngSpeed();
+		return speed[0];
+	}
+
+	public double xAngle(){
+		double[] angles  = I2CGyro.getAngles();
+		return angles[0];
+	}
+	public double yAngularSpeed(){
+		double[] speed = I2CGyro.getAngSpeed();
+		return speed[1];
+	}
+
+	public double yAngle(){
+		double[] angles  = I2CGyro.getAngles();
+		return angles[1];
+	}
 	public double zAngularSpeed(){
 		double[] speed = I2CGyro.getAngSpeed();
 		return speed[2];
 	}
-	
+
 	public double zAngle(){
 		double[] angles  = I2CGyro.getAngles();
 		return angles[2];
