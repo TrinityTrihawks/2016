008efa95291b643adc2486436d5bed2af793c33b
JAMESY9868 <abcde201220122012@outlook.com>
2016-03-10 17:14:35 -0600

    updates

--- src/main/java/frc/team4215/stronghold/I2CAccel.java
+++ src/main/java/frc/team4215/stronghold/I2CAccel.java
@@ -13,13 +13,13 @@ public class I2CAccel {
             ID = new byte[1];
     private static double[] accelVal = new double[3];
     private static Thread pingerThread;
-    
+
     public static void initAccel() {
         accel = new I2C(I2C.Port.kOnboard, 0x1D);
         // Accelerometer
         byte[] reg12 = new byte[1];
         accel.read(0x12, 1, reg12);
-
+        
         accel.write(CTRL_REG + 1, 0x57); // 0x20
         accel.write(CTRL_REG + 3, 0x0);// 4); //0x22
         accel.write(CTRL_REG + 4, 0x0);// 4); //0x23
@@ -27,7 +27,11 @@ public class I2CAccel {
         accel.write(CTRL_REG + 7, 0x00); // 0x26
         accel.read(WHO_AM_I, 1, ID);
     }
-    
+
+    public static void integrate() {
+        
+    }
+
     public static void pingAccel() {
         
         accel.read(OUT_REG, 1, buffL);
@@ -35,34 +39,34 @@ public class I2CAccel {
         accelVal[0] = concatCorrect(buffH[0], buffL[0]);
         accelVal[0] /= 1000;
         accelVal[0] *= G_IN_IPS2;
-
+        
         accel.read(OUT_REG + 2, 1, buffL);
         accel.read(OUT_REG + 3, 1, buffH);
         accelVal[1] = concatCorrect(buffH[0], buffL[0]);
         accelVal[1] /= 1000;
         accelVal[1] *= G_IN_IPS2;
-
+        
         accel.read(OUT_REG + 4, 1, buffL);
         accel.read(OUT_REG + 5, 1, buffH);
         accelVal[2] = concatCorrect(buffH[0], buffL[0]);
         accelVal[2] /= 1000;
         accelVal[2] *= G_IN_IPS2;
         RobotModule.logger.info("Accel: " + accelVal[1]);
-        
+
     }
-    
+
     public static double concatCorrect(byte h, byte l) {
         int high = Byte.toUnsignedInt(h);
         int low = Byte.toUnsignedInt(l);
         int test = ((0xFF & high) << 8) + (0xFF & low);
-
+        
         test = ((test > 0x7FFF) ? test - 0xFFFF : test);
         test *= coeff;
-
+        
         if (test > 65) return test;
         else return 0;
     }
-    
+
     public static void pingerStart() {
         Runnable pinger = () -> {
             while (pingFlag) {
@@ -74,19 +78,19 @@ public class I2CAccel {
                 }
             }
         };
-        
+
         pingerThread = new Thread(pinger);
         pingFlag = true;
         pingerThread.start();
-        
+
     }
-    
+
     public static double[] getAccel() {
         return accelVal;
     }
-    
+
     public static void pingerStop() {
         pingFlag = false;
     }
-    
+
 }
