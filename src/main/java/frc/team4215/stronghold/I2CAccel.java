9d3ced65819e40353c70457757150e2a738ef375
JAMESY9868 <abcde201220122012@outlook.com>
2016-03-11 06:24:23 -0600

    updates: change I2CAccel.pingAccel() 's return value
    The return value changes from void to double[] for convenience. Now it
    returns a copy of the accelVal.

--- src/main/java/frc/team4215/stronghold/I2CAccel.java
+++ src/main/java/frc/team4215/stronghold/I2CAccel.java
@@ -17,21 +17,21 @@ public class I2CAccel {
     private static double[] accelVal = new double[3];
     private static double[] velocVal = new double[3];
     private static double[] positVal = new double[3];
-
-    private static Thread pingerThread;
     
+    private static Thread pingerThread;
+
     /**
      * Should always size it as [x][3].
      */
     protected static double[][] velocity;
     protected static double[] position;
-
+    
     public static void initAccel() {
         accel = new I2C(I2C.Port.kOnboard, 0x1D);
         // Accelerometer
         byte[] reg12 = new byte[1];
         accel.read(0x12, 1, reg12);
-        
+
         accel.write(CTRL_REG + 1, 0x57); // 0x20
         accel.write(CTRL_REG + 3, 0x00);// 4); //0x22
         accel.write(CTRL_REG + 4, 0x00);// 4); //0x23
@@ -39,7 +39,7 @@ public class I2CAccel {
         accel.write(CTRL_REG + 7, 0x00); // 0x26
         accel.read(WHO_AM_I, 1, ID);
     }
-    
+
     public static void velInteg() {
         accel.read(I2CAccel.FIFO_SRC_REG, 1, buffL);
         final int loopCount = buffL[0] & 0x1f;
@@ -48,13 +48,13 @@ public class I2CAccel {
             pingAccel();
             accelList.add(accelVal);
         }
-
-    }
-    
-    public static void distInteg() {
         
     }
 
+    public static void distInteg() {
+        
+    }
+    
     /**
      * @param data
      *            Sized as [X][3]
@@ -64,42 +64,46 @@ public class I2CAccel {
         
         return null; // placeholder
     }
-
-    public static void pingAccel() {
+    
+    /**
+     * @return a copy of accelVal.
+     */
+    public static double[] pingAccel() {
         
         accel.read(OUT_REG, 1, buffL);
         accel.read(OUT_REG + 1, 1, buffH);
         accelVal[0] = concatCorrect(buffH[0], buffL[0]);
         accelVal[0] /= 1000;
         accelVal[0] *= G_IN_IPS2;
-        
+
         accel.read(OUT_REG + 2, 1, buffL);
         accel.read(OUT_REG + 3, 1, buffH);
         accelVal[1] = concatCorrect(buffH[0], buffL[0]);
         accelVal[1] /= 1000;
         accelVal[1] *= G_IN_IPS2;
-        
+
         accel.read(OUT_REG + 4, 1, buffL);
         accel.read(OUT_REG + 5, 1, buffH);
         accelVal[2] = concatCorrect(buffH[0], buffL[0]);
         accelVal[2] /= 1000;
         accelVal[2] *= G_IN_IPS2;
         RobotModule.logger.info("Accel: " + accelVal[1]);
-
+        
+        return accelVal;
     }
-
+    
     public static double concatCorrect(byte h, byte l) {
         int high = Byte.toUnsignedInt(h);
         int low = Byte.toUnsignedInt(l);
         int test = ((0xFF & high) << 8) + (0xFF & low);
-        
+
         test = ((test > 0x7FFF) ? test - 0xFFFF : test);
         test *= coeff;
-        
+
         if (test > 65) return test;
         else return 0;
     }
-
+    
     public static void pingerStart() {
         Runnable pinger = () -> {
             while (pingFlag) {
@@ -111,19 +115,19 @@ public class I2CAccel {
                 }
             }
         };
-
+        
         pingerThread = new Thread(pinger);
         pingFlag = true;
         pingerThread.start();
-
+        
     }
-
+    
     public static double[] getAccel() {
         return accelVal;
     }
-
+    
     public static void pingerStop() {
         pingFlag = false;
     }
-
+    
 }
