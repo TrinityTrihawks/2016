cfc7655f570425f23cf28cd081680c1e1b4ae400
Waweros <lion@waposta.com>
2016-03-15 17:24:16 -0500

    Made intake go in faster

--- src/main/java/frc/team4215/stronghold/Intake.java
+++ src/main/java/frc/team4215/stronghold/Intake.java
@@ -31,8 +31,8 @@ public class Intake {
      * The Motor, Victor, for controling the intake.
      */
     private Victor intake;
-    static double coeff = .75;
-    
+    static double inCoeff = .75;
+    static double outCoeff = 1;
     /**
      * Default constructor.
      */
@@ -48,10 +48,10 @@ public class Intake {
         
         if (gameCube
                 .getRawButton(Const.JoyStick.Button.GameCube_A))
-            intake.set(coeff*Const.Motor.Run.Forward);
+            intake.set(outCoeff*Const.Motor.Run.Forward);
         else if (gameCube
                 .getRawButton(Const.JoyStick.Button.GameCube_B))
-            intake.set(coeff*Const.Motor.Run.Backward);
+            intake.set(inCoeff*Const.Motor.Run.Backward);
         else intake.set(Const.Motor.Run.Stop);
     }
     public double get(){
