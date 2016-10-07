8b2233d20537cb15209647de00a0128feb53fcae
Waweros <lion@waposta.com>
2016-10-07 16:51:01 -0500

    Added accelration tracking

--- src/main/java/frc/team4215/stronghold/DataGather.java
+++ src/main/java/frc/team4215/stronghold/DataGather.java
@@ -1,5 +1,7 @@
 package frc.team4215.stronghold;
 
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import jaci.openrio.module.blackbox.BlackBox;
 import jaci.openrio.module.blackbox.BlackBoxContext;
 
@@ -9,7 +11,7 @@ import jaci.openrio.module.blackbox.BlackBoxContext;
  * @author Waweru
  */
 public class DataGather {
-
+	Accelerometer accel;
     BlackBoxContext context;
     DriveTrain chassis;
     UI ds;
@@ -19,16 +21,22 @@ public class DataGather {
         this.ds = ds;
         
         context = BlackBox.context("gathered_data.csv");
-        
+        accel = new BuiltInAccelerometer();
         // Logging the volts to the motors
         context.add("Volts: Right ", this::leftVolts);
         context.add("Volts: Left", this::rightVolts);
+        
         
         // Logging the inputs to the drive station
         context.add("Drive: Left", this::leftInputs);
         context.add("Drive: Right", this::rightInputs);
         
         context.add("Arm inputs", arm::getInput);
+        
+        //Logging acceleration
+        context.add("X acceleration", accel::getX);
+        context.add("Y acceleration", accel::getY);
+        context.add("Z acceleration", accel::getZ);
     }
     
     public void tick() {
